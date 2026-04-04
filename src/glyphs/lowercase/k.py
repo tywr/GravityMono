6dadb1dae83000bff17aa56f0886b1d1bc8552dc
tywr <tanguy.viv@gmail.com>
2026-04-04 10:43:02 +0200

    add: draw parallelogram

--- src/glyphs/lowercase/k.py
+++ src/glyphs/lowercase/k.py
@@ -1,7 +1,8 @@
-from math import atan, cos, sin, sqrt, asin, atan2
+from math import tan
 from glyph import Glyph
 from shapes.rect import draw_rect
 from shapes.polygon import draw_polygon
+from shapes.parallelogramm import draw_parallelogramm
 
 
 class LowercaseKGlyph(Glyph):
@@ -11,57 +12,24 @@ class LowercaseKGlyph(Glyph):
     bowl_width = 320
     neck_len = 100
     width_ratio = 1
-    neck_ratio = 0.35
+    branch_ratio = 0.75
 
     def draw(self, pen, dc):
         b = dc.body_bounds(offset=self.offset, width_ratio=self.width_ratio)
-        x_neck = b.x1 + self.neck_ratio * b.width
+        # x_neck = b.x1 + self.neck_ratio * b.width
+        x_branch = b.x1 + (1 - self.branch_ratio) * b.width
 
         # Angle of each diagonal branch
-        w = b.x2 - x_neck
-        h = dc.x_height / 2 - dc.stroke / 2
-        s = dc.stroke
-        delta = (s * (h * sqrt(w**2 + h**2 - s**2) - s * w)) / (h**2 - s**2)
-        theta = atan2(h, w - delta)
+        # w = b.x2 - x_neck
+        # h = dc.x_height / 2 - dc.stroke / 2
+        # s = dc.stroke
+        # delta = (s * (h * sqrt(w**2 + h**2 - s**2) - s * w)) / (h**2 - s**2)
+        # theta = atan2(h, w - delta)
         # delta = s / cos(theta)
 
         # Left ascender stem
         draw_rect(pen, b.x1, 0, b.x1 + dc.stroke, dc.ascent)
-        # Middle junction connecting stem to branches
-        draw_rect(
-            pen,
-            b.x1 + dc.stroke,
-            b.ymid - dc.stroke / 2,
-            x_neck,
-            b.ymid + dc.stroke / 2,
-        )
-        # Upper branch (stem to top-right)
-        draw_polygon(
-            pen,
-            points=[
-                (x_neck, dc.x_height / 2 + dc.stroke / 2),
-                (b.x2 - delta, dc.x_height),
-                (b.x2, dc.x_height),
-                (x_neck + delta, dc.x_height / 2 + dc.stroke / 2),
-                (
-                    x_neck + delta - dc.stroke * cos(theta),
-                    dc.x_height / 2 + dc.stroke / 2 - dc.stroke * sin(theta),
-                ),
-                (x_neck, dc.x_height / 2 - dc.stroke / 2),
-            ],
-        )
-        # Lower branch (stem to bottom-right)
-        draw_polygon(
-            pen,
-            points=[
-                (x_neck, dc.x_height / 2 + dc.stroke / 2),
-                (
-                    x_neck + delta - dc.stroke * cos(theta),
-                    dc.x_height / 2 - dc.stroke / 2 + dc.stroke * sin(theta),
-                ),
-                (x_neck + delta, dc.x_height / 2 - dc.stroke / 2),
-                (b.x2, 0),
-                (b.x2 - delta, 0),
-                (x_neck, dc.x_height / 2 - dc.stroke / 2),
-            ],
-        )
+        # Upper branch
+        draw_parallelogramm(pen, dc.stroke, x_branch, b.ymid + dc.stroke / 2, b.x2, b.y1, direction="bottom")
+        theta, delta = draw_parallelogramm(pen, dc.stroke, x_branch, b.ymid - dc.stroke / 2, b.x2, b.y2)
+        draw_rect(pen, b.x1, b.ymid - dc.stroke / 2, x_branch + delta / tan(theta), b.ymid + dc.stroke / 2)
---
+++ src/shapes/parallelogramm.py
@@ -0,0 +1,22 @@
+from math import sqrt, atan2
+from shapes.polygon import draw_polygon
+
+
+def draw_parallelogramm(pen, stroke, x1, y1, x2, y2, direction="top"):
+    w = max(x2, x1) - min(x2, x1)
+    h = max(y2, y1) - min(y2, y1)
+    s = stroke
+    delta = (s * (h * sqrt(w**2 + h**2 - s**2) - s * w)) / (h**2 - s**2)
+    theta = atan2(h, w - delta)
+    if direction == "top":
+        draw_polygon(
+            pen, points=[(x1, y1), (x1 + delta, y1), (x2, y2), (x2 - delta, y2)]
+        )
+        return theta, delta
+    elif direction == "bottom":
+        draw_polygon(
+            pen, points=[(x2 - delta, y2), (x2, y2), (x1 + delta, y1), (x1, y1)]
+        )
+        return theta, delta
+    else:
+        raise ValueError("Value should be either `top` or `bottom`")
