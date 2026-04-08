bfe46cd4659b7e840a5d51b7dd20f54de4e5c9b7
tywr <tanguy.viv@gmail.com>
2026-04-08 11:29:21 +0200

    adjust: 7

--- src/glyphs/numbers/seven.py
+++ src/glyphs/numbers/seven.py
@@ -6,7 +6,8 @@ from draw.parallelogramm import draw_parallelogramm
 class SevenGlyph(NumberGlyph):
     name = "seven"
     unicode = "0x37"
-    offset = 0
+    offset = 16
+    width_ratio = 1.1
 
     def draw(self, pen, dc):
         b = dc.body_bounds(
